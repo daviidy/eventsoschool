71868781f46cf5bf81328fa0e1fb51498faa4a5e
daviidy <yaodavidarmel@gmail.com>
2018-08-06 17:00:45 +0000

    changement slider 2

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -13,7 +13,7 @@
 
       <form method="post" enctype="multipart/form-data" action="https://secure.cinetpay.com/" class="login100-form validate-form">
         <span class="login100-form-title">
-          Le ticket coûte 15.000 FCFA
+          Le ticket coûte 10.000 FCFA
         </span>
         {{ csrf_field() }}
 
