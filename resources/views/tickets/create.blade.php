277404a4991c510ed3c0ce033e3dcc15ef746c9a
daviidy <yaodavidarmel@gmail.com>
2018-08-13 23:15:43 +0000

    changement prix

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -24,7 +24,7 @@
         </div>
         <div style="display: none;" class="form-group">
           <select class="" name="cpm_amount">
-            <option value="100">montant</option>
+            <option value="10000">montant</option>
           </select>
         </div>
         <div style="display: none;" class="form-group">
